b30ce03340bbfb5398cf1e6462d13c219186ebd4
Alex Karl Voskuil <avoskuil@andrew.cmu.edu>
2016-03-30 16:36:06 -0400

    added dig mechanics

---
+++ Momotaro/Assets/Scripts/DigBehavior.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DigBehavior : MonoBehaviour {
+
+	public GameObject dog;
+	public GameObject textbox;
+	public int horizontal;
+	public float forward;
+	public float limit;
+
+	private bool inDigZone;
+	private bool locked;
+	
+	// Use this for initialization
+	void Start () {
+		textbox.GetComponent<Text>().text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dogPos = dog.GetComponent<Transform>().position;
+		if(inDigZone && Input.GetKey (KeyCode.S) && !locked){
+			if(horizontal == 1){
+				transform.position = new Vector3 (transform.position.x+(0.1f*forward),transform.position.y,transform.position.z);
+			}
+			else if (horizontal == 0){
+				transform.position = new Vector3 (transform.position.x,transform.position.y-(0.1f*forward),transform.position.z);
+			}
+			else{
+				transform.position = new Vector3 (transform.position.x+(0.1f*forward),transform.position.y+(0.1f*forward),transform.position.z);
+			}
+		}
+		if((horizontal==0) && !locked){
+			if(((transform.position.y + 2) >= dogPos.x) && (Mathf.Abs(transform.position.x - 2) >= Mathf.Abs(dogPos.x))){
+				textbox.GetComponent<Text>().text = "Press S to dig down!";
+				inDigZone = true;
+			}
+			else{
+				textbox.GetComponent<Text>().text = "";
+				inDigZone = false;
+			}
+			if(transform.position.y <= limit){
+				locked = true;
+				GetComponent<MeshRenderer>().enabled = false;
+				GetComponent<BoxCollider>().enabled = false;
+			}
+		}
+		else if((horizontal==1) && !locked){
+			if(((transform.position.x - 2) <= dogPos.x) && (transform.position.x >= dogPos.x) && (transform.position.y >= dogPos.y)){
+				Debug.Log("hello?");
+				textbox.GetComponent<Text>().text = "Press S to dig across!";
+				inDigZone = true;
+			}
+			else{
+				textbox.GetComponent<Text>().text = "";
+				inDigZone = false;
+			}
+			if(transform.position.x >= limit){
+				locked = true;
+				GetComponent<MeshRenderer>().enabled = false;
+				GetComponent<BoxCollider>().enabled = false;
+			}
+		}
+		else if(!locked){
+			if(((transform.position.x - 2) <= dogPos.x) && (transform.position.x >= dogPos.x) && (transform.position.y >= dogPos.y)){
+				Debug.Log("hello?");
+				textbox.GetComponent<Text>().text = "Press S to dig across!";
+				inDigZone = true;
+			}
+			else{
+				textbox.GetComponent<Text>().text = "";
+				inDigZone = false;
+			}
+			if(transform.position.x >= limit){
+				locked = true;
+				GetComponent<MeshRenderer>().enabled = false;
+				GetComponent<BoxCollider>().enabled = false;
+			}
+		}
+	}
+}
---
+++ Momotaro/Assets/Scripts/DogBehavior.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogBehavior : MonoBehaviour {
+
+	public float velocity;
+	public float jumpForce;
+	private Rigidbody myRb;
+	private bool onSomething = false;
+	private bool underSomething = false;
+	private bool movingLeft;
+	private bool movingRight;
+	private bool jump = false;
+	private bool crouching = false;
+
+	// Use this for initialization
+	void Start () {
+
+
+		velocity = 10f;
+		jumpForce = 1000f;
+		myRb = GetComponent<Rigidbody> ();
+		myRb.freezeRotation = true;
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Interactable"));
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Player"));
+	}
+
+	void Update() {
+		Vector3 right = transform.position + Vector3.right * transform.lossyScale.x * 0.5f;
+		Vector3 left = transform.position - Vector3.right * transform.lossyScale.x * 0.5f;
+
+		Debug.DrawLine (right, right + (Vector3.down * transform.lossyScale.y * 1));
+		Debug.DrawLine (left, left + (Vector3.down * transform.lossyScale.y * 1));
+		Debug.DrawLine (right, right + (Vector3.up * 1));
+		Debug.DrawLine (left, left + (Vector3.up * 1));
+
+		onSomething = Physics.Linecast (right, right + (Vector3.down * transform.lossyScale.y * 1), 1 << LayerMask.NameToLayer ("Obstacle")) 
+			|| Physics.Linecast (left, left + (Vector3.down * transform.lossyScale.y * 1), 1 << LayerMask.NameToLayer ("Obstacle"));
+
+		underSomething = Physics.Linecast (right, right + (Vector3.up * 1), 1 << LayerMask.NameToLayer ("Obstacle")) 
+			|| Physics.Linecast (left, left + (Vector3.up * 1), 1 << LayerMask.NameToLayer ("Obstacle"));
+
+		movingLeft = false;
+		movingRight = false;
+
+		if (Input.GetKey (KeyCode.A)) {
+			movingLeft = true;
+			Vector3 s = transform.localScale;
+			s.x = -1;
+			transform.localScale = s;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			movingRight = true;
+			Vector3 s = transform.localScale;
+			s.x = 1;
+			transform.localScale = s;
+		}
+
+
+
+		if (Input.GetKeyDown(KeyCode.Space) && onSomething && !crouching) {
+			jump = true;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			crouching = true;
+		}
+		if (!Input.GetKey (KeyCode.S) && !underSomething) {
+			crouching = false;
+		}
+
+
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (movingLeft) {
+			//restrict movement to one plane
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+			myRb.velocity = new Vector3 (-1 * velocity, myRb.velocity.y, myRb.velocity.z);
+		}
+		if (movingRight) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+			myRb.velocity = new Vector3 (velocity, myRb.velocity.y, myRb.velocity.z);
+		}
+
+		if (crouching) {
+			transform.localScale = new Vector3 (transform.localScale.x, 0.5f, 1f);
+			velocity = 5f;
+		}
+		else {
+			transform.localScale = new Vector3 (transform.localScale.x, 1f, 1f);
+			velocity = 10f;
+		}
+
+		if (jump && onSomething) {
+			myRb.AddForce (Vector3.up * jumpForce);
+			jump = false;
+		}
+		if (!movingRight && !movingLeft) {
+			myRb.velocity = new Vector3(0f, myRb.velocity.y, myRb.velocity.z);
+		}
+		transform.rotation = Quaternion.Euler (Vector3.zero);
+	}
+}
