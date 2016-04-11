b28e492a66fd1f54e32ce2ef595bb52b08123750
Alex Karl Voskuil <avoskuil@andrew.cmu.edu>
2016-04-11 11:22:37 -0400

    add instructions

---
+++ Momotaro/Assets/Scripts/AllInstructions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class AllInstructions : MonoBehaviour {
+
+	public string[] messages;
+	public bool[] isthere;
+	public int number;
+	public GameObject box;
+	public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool first = isthere[0];
+		for(int i = 1; i < isthere.Length; i++){
+			first = first || isthere[i];
+		}
+		if(first){
+			box.SetActive(true);
+			text.GetComponent<Text>().text = messages[number];
+		}
+		else{
+			box.SetActive(false);
+			text.GetComponent<Text>().text = "";
+		}
+	}
+}
---
+++ Momotaro/Assets/Scripts/Instructions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Instructions : MonoBehaviour {
+
+	public int num;
+	public string message;
+	public GameObject instruct; 
+	public GameObject momo;
+
+	// Use this for initialization
+	void Start () {
+		instruct.GetComponent<AllInstructions> ().messages[num] = message;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((momo.GetComponent<Transform>().position.x <= transform.position.x) &&
+		   (momo.GetComponent<Transform>().position.x + 10 >= transform.position.x)){
+			instruct.GetComponent<AllInstructions> ().isthere[num] = true;
+			instruct.GetComponent<AllInstructions> ().number = num;
+			
+		}
+		else{
+			instruct.GetComponent<AllInstructions> ().isthere[num] = false;
+		}
+	}
+}
