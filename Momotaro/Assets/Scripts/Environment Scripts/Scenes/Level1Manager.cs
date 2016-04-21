381f8a667f49fccb9282b0fde1659048ccc495b9
Alex Karl Voskuil <avoskuil@andrew.cmu.edu>
2016-04-21 00:34:33 -0400

    level 1 updated cut-scenes

--- Momotaro/Assets/Scripts/Environment Scripts/Scenes/Level1Manager.cs
+++ Momotaro/Assets/Scripts/Environment Scripts/Scenes/Level1Manager.cs
@@ -9,18 +9,77 @@ public class Level1Manager : MonoBehaviour {
 
 	public GameObject[] onis;
 	public GameObject dog;
+	public GameObject cam;
+	public GameObject momo;
 	public int scene;
 	public bool[] onisKilled;
+	public AudioSource companionSong;
+	
+	private bool move1;
+	private bool slowdown;
+	private float startTime;
 
 	// Use this for initialization
 	void Start () {
-		
+		companionSong = gameObject.GetComponent<AudioSource>();
+		companionSong.volume = 0f;
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//for(int i=0;i++;i<onis.Length){
-		//	onis[i].GetComponent<Rigidbody> ();
-		//}
+		if(scene ==0){
+			move1 = true;
+			for(int i=0; i<onis.Length ;i++){
+				onisKilled[i] = onis[i].GetComponent<DerpBehavior> ().isDead;
+				move1 = onisKilled[i] && move1;
+			}
+		}
+		Debug.Log(scene);
+		if(scene == 2){
+			Debug.Log(momo.GetComponent<Transform> ().position.x);
+			if(momo.GetComponent<Transform> ().position.x >= 88f &&
+			   dog.GetComponent<Transform> ().position.x < 90f){
+				dog.GetComponent<LevelOneDialogue> ().moveDog = true;
+			}
+			else if(dog.GetComponent<Transform> ().position.x >= 90f){
+				dog.GetComponent<LevelOneDialogue> ().moveDog = false;
+				scene ++;
+				dog.GetComponent<LevelOneDialogue> ().scene = scene;
+			}
+			if(momo.GetComponent<Transform> ().position.x >= 93f){
+				momo.GetComponent<MomotaroBehavior> ().stop = true;
+			}
+		}
+		
+		
+		if(move1){
+			slowdown = true;
+			scene ++;
+			move1 = false;
+			dog.GetComponent<LevelOneDialogue> ().scene = scene;
+		}
+		if(slowdown && ((Time.time - startTime) > 0.1f) && dog.GetComponent<LevelOneDialogue> ().seesMomo){
+			startTime = Time.time;
+			cam.GetComponent<AudioSource> ().volume -= 0.1f;
+			if(cam.GetComponent<AudioSource> ().volume == 0){
+				slowdown = false;
+			}
+		}
+		if((dog.GetComponent<LevelOneDialogue> ().count >= 2) && ((Time.time - startTime) > 0.1f) &&
+		   (dog.GetComponent<LevelOneDialogue> ().scene == 1)){
+			//Debug.Log("wowie");
+			startTime = Time.time;
+			companionSong.volume += 0.1f;
+		}
+		else if((dog.GetComponent<LevelOneDialogue> ().scene == 2) && ((Time.time - startTime) > 0.1f) &&
+				cam.GetComponent<AudioSource> ().volume < 0.99f){
+				startTime = Time.time;
+				cam.GetComponent<AudioSource> ().volume += 0.1f;
+				companionSong.volume -= 0.1f;
+				scene = 2;
+		}
+		
+		
 	}
 }
--- Momotaro/Assets/Scripts/Environment Scripts/Scenes/LevelOneDialogue.cs
+++ Momotaro/Assets/Scripts/Environment Scripts/Scenes/LevelOneDialogue.cs
@@ -6,51 +6,53 @@ using UnityEngine.SceneManagement;
 
 public class LevelOneDialogue : MonoBehaviour {
 
-	public bool hasDefeatOni;
+	public int scene;
+	public GameObject momo;
+	public GameObject[] boxes;
+	public GameObject textbox;
+	public string[] messages;
+	public float refract = 0.5f;
+	
+	public int count;
+	private float startTime;
+	private float jumpTime;
+	
 	public float velocity;
 	public float jumpForce;
-	public bool jump = false;
-
-	public GameObject dogs;
-	public GameObject momo;
-	public GameObject text;
-	public GameObject box;
-	public GameObject[] dogDiag;
-	public GameObject[] momoDiag;
-	public AudioSource[] voices;
-	public AudioSource[] music;
+	//public bool jump = false;
 	
-	private bool onSomething;
+	//private bool onSomething;
 	public bool moveDog;
 	private Rigidbody dogRb;
-	private CapsuleCollider myCollider;
-	private bool isDone;
+	//private CapsuleCollider myCollider;
 	
-	private bool seesMomo;
-	private int count;
-	private int dogIdx = -1;
-	private int momoIdx = -1;
-	private int diagLimit = 20;
-	public bool stop;
-	private bool isMomo;
-	private float refractTime;
-	private float refract = 0.15f;
-	private float startTime;
-	private float pause = 0.5f;
-	private float jumpTime;
-	private int wordCount;
+	public bool seesMomo;
 	
 	// Use this for initialization
 	void Start () {
-		voices = gameObject.GetComponents<AudioSource>();
-		startTime = Time.time;
-		refractTime = Time.time;
-		jumpTime = Time.time;
 		dogRb = GetComponent<Rigidbody> ();
-		//myCollider = gameObject.GetComponent<CapsuleCollider> ();
 		dogRb.freezeRotation = true;
 		velocity = 10f;
 		jumpForce = 1000f;
+		messages[0] = "*huff* *huff*";
+		messages[1] = "Aw, poor dog… Are you okay?";
+		messages[2] = "That. Was. Awesome!";
+		messages[3] = "Wh-whoah! You can talk?!";
+		messages[4] = "One, yes. Two, you saved me from those Oni! And three, you are now my BEST FRIEND!";
+		messages[5] = "Well, um. You’re welcome! I’m on a mission. To fight the Oni. And also maybe find out where I came from.";	
+		messages[6] = "Amazing! So, where are we going next?";
+		messages[7] = "We? I don’t know how to put this, but… I’m more of a solo hero.";
+		messages[8] = "Oh. Oh! Of course. Yes. I… understand. Well. Goodbye then.";
+		messages[9] = "Goodbye… uh… what’s your name?";
+		messages[10] = "Dog.";
+		messages[11] = "Okay then. I’m Momotaro, but you can call me Momo."; 
+		messages[12] = "Momo."; 
+		messages[13] = "Yes. Well, goodbye then, Dog!";
+		messages[14] = "Goodbye, Momo!";
+		boxes[0].SetActive(false);
+		boxes[1].SetActive(false);
+		startTime = Time.time;
+		jumpTime = Time.time;
 	}
 	
 	// Update is called once per frame
@@ -58,35 +60,51 @@ public class LevelOneDialogue : MonoBehaviour {
 		if(Input.GetKey(KeyCode.X)){
 			SceneManager.LoadScene ("AlphaDemo");
 		}
+		if((momo.GetComponent<Transform>().position.x <= transform.position.x) &&
+		   (momo.GetComponent<Transform>().position.x + 2 >= transform.position.x)){
+			seesMomo = true;
+			//box1.SetActive(true);
+			//text1.GetComponent<Text>().text = "Press 'Enter' to Eat Breakfast";
+		}
+		if(scene ==1 && seesMomo){
+			momo.GetComponent<MomotaroBehavior> ().stop = true;
+			if((count % 2) ==0){
+				boxes[0].SetActive(true);
+				boxes[1].SetActive(false);
+			}
+			else{
+				boxes[1].SetActive(true);
+				boxes[0].SetActive(false);
+			}
+			textbox.GetComponent<Text>().text = messages[count];
+			if(Input.GetKey(KeyCode.Return) &&((Time.time-startTime) > refract)){
+				startTime = Time.time;
+				count ++;
+				if(count >= 15){
+					scene ++;
+					momo.GetComponent<MomotaroBehavior> ().stop = false;
+				}
+			}
+		}
 	}
 	
 	void FixedUpdate () {
 		
 		if (moveDog) {
+			transform.rotation = new Quaternion (0f, 0f, 0f,0f);
 			//restrict movement to one plane
 			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
 			dogRb.velocity = new Vector3 (velocity, dogRb.velocity.y, dogRb.velocity.z);
 			Vector3 s = transform.localScale;
 			s.x = 1;
 			transform.localScale = s;
-			if((transform.position.x >= 131f && count < 20) || 
-			   (transform.position.x >= 179f)){
-				moveDog = false;
-				if(count < 20){
-					count = 17;
-					dogs.GetComponent<Transform> ().position = new Vector3 (30f,transform.position.y,transform.position.z);
-				}
-				else{
-					Debug.Log("yo");
-					count = 23;
-					dogs.GetComponent<Transform> ().position = new Vector3 (75f,transform.position.y,transform.position.z);
-				}
-			}
+			//dogs.GetComponent<Transform> ().position = new Vector3 (75f,transform.position.y,transform.position.z);
+	
 			if((Time.time - jumpTime) >= 1f){
 				Debug.Log("Jump!");
 				transform.position = new Vector3 (transform.position.x, transform.position.y+1f, 0f);
 				jumpTime = Time.time;
-				//dogRb.AddForce (Vector3.up * jumpForce);
+				dogRb.AddForce (Vector3.up * jumpForce);
 			}
 		}
 		velocity = 10f;
