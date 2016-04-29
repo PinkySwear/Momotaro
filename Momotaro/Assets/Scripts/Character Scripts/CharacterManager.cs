3b4d9801b33c444d9e1cc580dca3de58af2024c0
Alex Karl Voskuil <avoskuil@andrew.cmu.edu>
2016-04-29 09:03:10 -0400

    level switching edit

--- Momotaro/Assets/Scripts/Character Scripts/CharacterManager.cs
+++ Momotaro/Assets/Scripts/Character Scripts/CharacterManager.cs
@@ -60,29 +60,29 @@ public class CharacterManager : MonoBehaviour {
 	void Update () {
 
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			SceneManager.LoadScene (0);
+			SceneManager.LoadScene ("FinalLevel0");
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			SceneManager.LoadScene (1);
+			SceneManager.LoadScene ("FinalLevel1");
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			SceneManager.LoadScene (2);
+			SceneManager.LoadScene ("FinalLevel2");
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha4)) {
-			SceneManager.LoadScene (3);
+			SceneManager.LoadScene ("FinalLevel3");
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha5)) {
-			SceneManager.LoadScene (4);
+			SceneManager.LoadScene ("Menu");
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha6)) {
-			SceneManager.LoadScene (5);
+			SceneManager.LoadScene ("Credits");
 		}
-		if (Input.GetKeyDown (KeyCode.Alpha7)) {
-			SceneManager.LoadScene (6);
-		}
-		if (Input.GetKeyDown (KeyCode.Alpha8)) {
-			SceneManager.LoadScene (7);
-		}
+		//if (Input.GetKeyDown (KeyCode.Alpha7)) {
+		//	SceneManager.LoadScene (6);
+		//}
+		//if (Input.GetKeyDown (KeyCode.Alpha8)) {
+		//	SceneManager.LoadScene (7);
+		//}
 		//if (momo.controlling)
 			//trianglePos = momo.transform.position + Vector3.up * 2.5f;
 		//else if (dog.controlling)
